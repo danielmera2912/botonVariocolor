5402e7c5d0b403ff23d40d44d65a997564e27878
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2021-12-25 17:01:24 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name='botonVariocolor',
+    packages=['botonVariocolor'],
+    version='0.1',
+    license='LGPL v3',
+    description='A random test lib',
+    author='danielmera2912',
+    author_email='dmersac2912@g.educaand.es',
+    url='https://github.com/danielmera2912/botonVariocolor',
+    download_url='https://github.com/danielmera2912/parallel_foreach_submodule/archive/v0.1.tar.gz',
+    keywords='test example develop', # Palabras que definan tu paquete
+    classifiers=['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paquete
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
+)
