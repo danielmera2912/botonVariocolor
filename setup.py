071337cc8fcd60249f718c1f3f6e61111f032832
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2022-01-11 18:55:22 +0100

    cambio

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='danielmera2912',
     author_email='dmersac2912@g.educaand.es',
     url='https://github.com/danielmera2912/botonVariocolor',
-    download_url='https://github.com/danielmera2912/parallel_foreach_submodule/archive/v0.1.tar.gz',
+    download_url='https://github.com/danielmera2912/botonVariocolor/archive/refs/tags/0.1.tar.gz',
     keywords='test example develop', # Palabras que definan tu paquete
     classifiers=['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paquete
                  'Programming Language :: Python :: 2.7',
